e7f65825e0f897cfb2d246576968efb65336ce2a
Arjun C Vinod <arjuncvinod007@gmail.com>
2023-12-16 22:57:17 +0530

    useCallback added

---
+++ src/useCallback/Child.jsx
@@ -0,0 +1,11 @@
+import React, { useEffect } from "react";
+
+function Child({ returnComment }) {
+  useEffect(() => {
+    console.log("FUNCTION WAS CALLED");
+  }, [returnComment]);
+
+  return <div>{returnComment(" Arjun")}</div>;
+}
+
+export default Child;
