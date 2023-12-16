d0aeb570881b8fff3b89187d65780c5697dce386
Arjun C Vinod <arjuncvinod007@gmail.com>
2023-12-16 22:57:12 +0530

    useCallback added

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import UseState from "./useState/UseState";
 import UseLayoutEffect from "./useLayoutEffect/UseLayoutEffect";
 import UseImperativeHandle from "./useImperativeHandle/UseImperativeHandle";
 import UseContext from "./useContext/UseContext";
+import UseCallBack from "./useCallback/UseCallback";
 function App() {
   return (
     <div>
@@ -15,6 +16,7 @@ function App() {
       <UseLayoutEffect />
       <UseImperativeHandle />
       <UseContext />
+      <UseCallBack />
     </div>
   );
 }
